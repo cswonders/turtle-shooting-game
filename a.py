a197962158c1758326dcef9145d9d70c48ac477a
Nightmerp <43102229+Nightmerp@users.noreply.github.com>
2018-09-09 19:24:45 -0700

    Update a.py

--- a.py
+++ a.py
@@ -1,14 +1,15 @@
-import turtle.Turtle
-class Player(Turtle)
+from turtle import Turtle, Screen
+class Player(Turtle):
     def __init__(self, wn):
         self.wn = wn
 
-    def turn_right():
+    def turn_right(self):
         self.right(10)
 
-    def turn_left():
+    def turn_left(self):
         self.left(10)
 
-    self.wn.onkey(turn_right, "Right")
-    self.wn.onkey(turn_left, "Left")
-    self.wn.listen()
+    def run(self):
+        self.wn.onkey(self.turn_right, "Right")
+        self.wn.onkey(self.turn_left, "Left")
+        self.wn.listen()
