9c1a17675a451680d7b9f2feb8f94e316e846e50
Turtlespac <32421701+spacepotatoturtle@users.noreply.github.com>
2018-09-15 20:04:51 -0700

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,29 +1,16 @@
 from turtle import Turtle
 import time
 class Bullet(Turtle):
-    def __init__(self):
+    def __init__(self, wn, xi, yi, speed):
         super().__init__()
+        self.ht()
+        self.wn = wn
+        self.goto(xi, yi)
+        self.st()
+        self.speed = speed
+       
+    def main()
+        self.forward(self.speed)
+        self.wn.ontimer(self.main, 5)
     
-    def spawn(size,lposy,lposx,hop):
-        self.tracer(0, 0)
-        self.up()
-        self.move(lposy,lposx)
-        self.color("white")
-        self.down()
-        self.start_fill()
-        self.circle(size)
-        self.stop_fill()
-        self.up()
-        self.forward(lposx)
-        self.left(90)
-        self.forward(lposy)
-        self.right(90)
-        self.forward(hop)
-        self.down()
-        self.start_fill()
-        self.color("blue")
-        self.circle(size)
-        self.stop_fill()
-        self.up()
-        self.update()
-        return self.xcor(), self.ycor()
+    self.main()
