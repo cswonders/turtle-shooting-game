47735117c6c4cffac07858db00eccee7a1751129
Turtlespac <32421701+spacepotatoturtle@users.noreply.github.com>
2018-09-15 19:08:03 -0700

    Update player.py

--- player.py
+++ player.py
@@ -5,6 +5,7 @@ class Player(Turtle):
         super().__init__()
         self.wn = wn
         self.t = Turtle()
+        self.cannon = bullet.Bullet()
 
     def turn_right(self):
         self.right(10)
@@ -16,14 +17,12 @@ class Player(Turtle):
         self.forward(10)
     
     def reverse(self):
-        self.backward(7)
-
-    self.gun = bullet.bullet    
+        self.backward(7)   
     
     def run(self):
         self.wn.onkey(self.turn_right, "Right")
         self.wn.onkey(self.turn_left, "Left")
         self.wn.onkey(self.drive, "Up")
         self.wn.onkey(self.reverse, "Down")
-        self.wn.onkey(self.gun.shoot, "space")
+        self.wn.onkey(self.cannon.shoot, "space")
         self.wn.listen()
