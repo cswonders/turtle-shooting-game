1b43455736aaa3b89fbcdd2a409747c589b7bee2
Nightmerp <43102229+Nightmerp@users.noreply.github.com>
2018-09-15 21:01:41 -0700

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,13 +1,14 @@
 from turtle import Turtle
 import time
 class Bullet(Turtle):
-    def __init__(self, wn, xi, yi, speed):
+    def __init__(self, wn, xi, yi, speed, direction):
         super().__init__()
         self.ht()
         self.wn = wn
         self.goto(xi, yi)
         self.st()
         self.speed = speed
+        self.setheading(direction)
        
     def run()
         self.forward(self.speed)
