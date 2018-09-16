848ded19320cf1f388b377fd2430200eb9cf0ac1
Nightmerp <43102229+Nightmerp@users.noreply.github.com>
2018-09-15 18:25:35 -0700

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,13 +1,17 @@
 from turtle import Turtle, Screen
 class Player(Turtle):
     def __init__(self, wn):
+        super().__init__()
         self.wn = wn
+        self.t = Turtle()
 
     def turn_right(self):
         self.right(10)
+        print('Left')
 
     def turn_left(self):
         self.left(10)
+        print('Right')
 
     def run(self):
         self.wn.onkey(self.turn_right, "Right")
